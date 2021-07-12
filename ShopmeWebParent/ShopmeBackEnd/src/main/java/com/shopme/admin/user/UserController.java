16cb73d9d832b680ec783e32dc7d13a198a63d91
HefnerK <kylethefner@gmail.com>
2021-07-12 12:27:11 -0700

    added Spring security to encode user password

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -40,7 +40,6 @@ public class UserController {
 	
 	@PostMapping("/users/save")
 	public String saveUser(User user, RedirectAttributes redirectAttributes) {
-		System.out.println(user);
 		service.save(user);
 		
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -88,4 +88,12 @@ public class UserRepositoryTests {
 		Integer userId = 2;
 		repo.deleteById(userId);;
 	}
+	
+	@Test
+	public void testGetUserByEmail() {
+		String email = "Matt@gmail.com";
+		User user = repo.getUserByEmail(email);
+		
+		assertThat(user).isNotNull();
+	}
 }
